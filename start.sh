b0d670837ae520f4182c1b87885a4e38cbf88620
Priyansh Arora <145475854+PriyanshAroraa@users.noreply.github.com>
2025-06-01 05:51:29 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# start.sh
+exec gunicorn app.main:app \
+  --workers 4 \
+  --worker-class uvicorn.workers.UvicornWorker \
+  --bind 0.0.0.0:$PORT \
+  --timeout 120 \
+  --log-level info
